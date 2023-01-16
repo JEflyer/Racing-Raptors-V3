f9e1fc3b35143482ea8dce7c67df4da43bc1f9df
Jonny <Email@Email.com>
2023-01-16 13:01:01 +0000

    Writing signature verifier

---
+++ contracts/SignatureVerifier.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: GLWTPL
+pragma solidity 0.8.15;
+
+contract SignatureVerifier {
+    error NullAddress();
+    error NotAdmin();
+
+    address private admin;
+
+    address private permissionGiver;
+
+    mapping (address => uint256) public currentNonce;
+
+    constructor(address _permissionGiver) {
+        if(_permissionGiver == address(0)) revert NullAddress();
+        
+        admin = msg.sender;
+        permissionGiver = _permissionGiver;
+    }
+
+    modifier onlyAdmin{
+        if(msg.sender != admin) revert NotAdmin();
+        _;
+    }
+
+    modifier NotNullAddress(address addr){
+        if(addr == address(0)) revert NullAddress();
+        _;
+    }
+
+    function setAdmin(address _new) external onlyAdmin NotNullAddress(_new) {
+        admin = _new;
+    }
+
+    function setPermissionGiver(address _new) external onlyAdmin NotNullAddress(_new){
+        permissionGiver = _new;
+    }
+
+    function verfySignature(uint8 v, bytes32 r, bytes32 s, address query) external returns(bool){
+        uint256 nonce = currentNonce[query]; 
+        
+        bytes32 hashedMessage = keccak256(query,nonce);
+
+        address signer = ecrecover(hashedMessage,v,r,s);
+
+        if(signer == permissionGiver){
+            currentNonce[query]++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
